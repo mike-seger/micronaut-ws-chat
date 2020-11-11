22bf080551c827a6ac8bdab3c26d45c11acc4ef6
Michael Seger <michael@macbookpro>
2020-11-11 18:33:12 +0100

    added WS test client

--- src/main/resources/public/assets/wsLogic.js
+++ src/main/resources/public/assets/wsLogic.js
@@ -63,7 +63,6 @@ function clearHistory() {
 
 function submitOnEnter(event){
     if(event.which === 13 && !event.shiftKey) {
-        //event.target.form.dispatchEvent(new Event("submit", {cancelable: true}));
         sendMessage();
         event.preventDefault(); // Prevents the addition of a new line in the text field (not needed in a lot of cases)
     }
